e984f1678aeee11c7ada6016d72dfe27669a202c
Galillei <bazilio.rodger@gmail.com>
2015-02-12 08:21:25 +0300

    работа в основном со стилями, и подготовкой формы к заполнению

--- public/controllers/CalculatorController.js
+++ public/controllers/CalculatorController.js
@@ -5,13 +5,31 @@
     CalculatorController.$inject = ['$scope'];
     function CalculatorController(){
         var vm = this;
-        vm.expession='12';
-        vm.method = 1;
+        vm.method = 2;
         vm.actions = {
-            1:'/',
             2:'+',
             3:'*',
             4:'-'
         }
+        vm.randomExpession = randomExpession;
+        vm.expession= vm.randomExpession();
+        vm.resetExpession = resetExpession;
+
+
+        function randomExpession()
+        {
+            var method = vm.actions[vm.method];
+            var firstNumber = Math.floor((Math.random() * 10) + 1);
+            var secondNumber = Math.floor((Math.random() * 10) + 1);
+            return firstNumber+method+secondNumber;
+        }
+
+        function resetExpession(){
+            vm.expession = vm.randomExpession();
+        }
+
+
+
+
     }
 })();
